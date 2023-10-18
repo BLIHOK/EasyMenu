150d2129c046ad38173f639d4e9c38c43a4cb4f2
Denis Aksenov <BLIHOK@mail.ru>
2023-10-18 21:15:18 +0300

    change README

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,23 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
+import java.sql.DriverManager
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+fun main(args: Array<String>) {
+
+    val url = "jdbc:sqlite:C:/SQLite/projects/menu.db"
+    DriverManager.getConnection(url).use { connection ->
+        val statement = connection.createStatement()
+
+        for (i in 2..6) {
+            val rnd = (0..3).random()
+            val resultSetRow = statement.executeQuery("SELECT * FROM FoodMenu LIMIT $rnd, 1")
+            val metaData = resultSetRow.metaData
+            val columnName = metaData.getColumnName(i)
+
+            if (resultSetRow.next()) {
+                val value = resultSetRow.getString(i)
+                println("Значение для столбца \"$columnName\": $value")
+            } else {
+                println("Строка $rnd не найдена")
+            }
+        }
+    }
 }
